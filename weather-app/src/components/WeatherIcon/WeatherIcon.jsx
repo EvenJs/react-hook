8f1bf5bfe83848407b5aaee39660eedbeb13a5b5
EvenW <yuhang0229@gmail.com>
2021-09-12 11:58:11 +1000

    add WeatherIcon component dispay icon and weather

---
+++ weather-app/src/components/WeatherIcon/WeatherIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function WeatherIcon({ imageUrl, weatherDescription }) {
+  return (
+    <div>
+      <img src={imageUrl} alt='weahter-icon' />
+      <h3>{weatherDescription}</h3>
+    </div>
+  )
+}
+
+export default WeatherIcon;
