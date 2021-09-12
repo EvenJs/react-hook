161ea8559c697176f8c485cab75c7b002b6714eb
EvenW <yuhang0229@gmail.com>
2021-09-12 11:57:25 +1000

    add WeatherDetail component display four type of information

---
+++ weather-app/src/components/WeatherDetail/WeatherDetail.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const getType = (type) => ({
+  humidity: 'Humidity',
+  wind_speed: 'Wind Speed',
+  sunrise: 'Sunrise Time',
+  sunset:'Sunset Time',
+})
+
+const getPrefix = (type) => ({
+  humidity: '%',
+  wind_speed: 'kms/h',
+  sunrise: '',
+  sunset: '',
+}[type])
+
+function WeatherDetail({ type, value }) {
+  return (
+    <div>
+      <h3>{getType(type)}</h3>
+      <div>{`${value} ${getPrefix(type)}`} </div>
+    </div>
+  );
+}
+
+export default WeatherDetail;
